b06a8954f1b5721dc60f7f19f907406b19beab14
DikeshShrestha <dikesh.shrestha.010@gmail.com>
2022-10-09 19:35:54 +0545

    .

---
+++ carFleet.py
@@ -0,0 +1,11 @@
+# accumulate speed with position in every iteration until target is reached
+# make same speed of cars with same position
+# count the number of group of cars that has position  == target
+
+def carFleet(target, position, speed):
+    
+    pass
+
+
+obj = carFleet(12, [10,8,0,5,3], [2,4,1,1,3])
+print(obj)
