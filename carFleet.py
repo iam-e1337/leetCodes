bd9906356dcec846f8e0246c034da268cdd6e249
DikeshShrestha <dikesh.shrestha.010@gmail.com>
2022-10-10 08:38:43 +0545

    .

--- carFleet.py
+++ carFleet.py
@@ -3,8 +3,25 @@
 # count the number of group of cars that has position  == target
 
 def carFleet(target, position, speed):
-    a
-    pass
+    allFleet = False
+    newPos = position.copy()
+    while allFleet is False:
+        length = len(newPos)
+
+        for i in range(len(newPos)):
+            current = newPos[i]
+            k = 1
+            for j in range(len(newPos)):
+                if newPos[j] == current:
+                    newPos[j] = k
+                else:
+                    k+=1
+
+        for i in range(length):
+            newPos[i]+= speed[i]
+
+        allFleet = True
+        return newPos
 
 
 obj = carFleet(12, [10,8,0,5,3], [2,4,1,1,3])
